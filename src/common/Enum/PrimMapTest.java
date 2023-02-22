19f2642ed62b6b11eaa785579552e573fe56a897
LiamVT2021 <liam.snyder.1998@gmail.com>
2023-02-22 17:56:51 -0500

    Start of a PrimMapTest class

---
+++ src/common/Enum/PrimMapTest.java
@@ -0,0 +1,46 @@
+package common.Enum;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class PrimMapTest {
+
+    private enum Type {
+        A, B, C, D;
+    }
+
+    private PrimMap.Bool<Type> boolMap;
+    private PrimMap.Byte<Type> byteMap;
+    private PrimMap.Int<Type> intMap;
+
+    @BeforeEach
+    public void setUp() {
+        boolMap = new PrimMap.Bool<>(Type.class);
+        byteMap = new PrimMap.Byte<>(Type.class);
+        intMap = new PrimMap.Int<>(Type.class);
+    }
+
+    private void forEach(Consumer<PrimMap<Type, ?, ?>> func) {
+        Stream.of(boolMap, byteMap, intMap).forEach(func);
+    }
+
+    @Test
+    public void testEnumMethods() {
+        forEach(map -> {
+            assertFalse(map.isEmpty());
+            assertEquals(4, map.size());
+            assertTrue(map.containsKey(Type.A));
+            assertFalse(map.containsKey("A"));
+            // assertTrue(Type.values() == map.keys());
+        });
+    }
+
+    public void testBoolMethods() {
+        
+    }
+
+}
