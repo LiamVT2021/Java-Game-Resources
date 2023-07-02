8768d925522c5eea2f464f2da2fd0ece9d0ae478
LiamVT2021 <liam.snyder.1998@gmail.com>
2023-07-02 15:47:22 -0400

    Java comments for bag

--- src/main/java/common/pushPop/Bag.java
+++ src/main/java/common/pushPop/Bag.java
@@ -3,6 +3,14 @@ package common.pushPop;
 import java.util.Iterator;
 import java.util.Random;
 
+/**
+ * A PushPop data structure that removes elements by random
+ * 
+ * @param G the type returned by get methods
+ * @param S the type consumed by set methods
+ * @param A the type of the wrapped array
+ * @version 7/2/23
+ */
 public abstract class Bag<G extends S, S, A> extends PushPop.Array<G, S, A> {
 
     private static final Random rand = new Random();
@@ -23,13 +31,21 @@ public abstract class Bag<G extends S, S, A> extends PushPop.Array<G, S, A> {
         return rand.nextInt(size);
     }
 
+    /**
+     * @return a random element from the bag, null if empty
+     */
     @Override
     public G peek() {
-        return array.get(draw());
+        return isEmpty() ? null : array.get(draw());
     }
 
+    /**
+     * removes a random element from the bag
+     */
     @Override
     public G pop() {
+        if (isEmpty())
+            return null;
         int i = draw();
         G ret = array.get(i);
         array.set(i, array.remove(--size));
@@ -38,7 +54,9 @@ public abstract class Bag<G extends S, S, A> extends PushPop.Array<G, S, A> {
 
     @Override
     public G swap(S value) {
-        return array.swap(draw(), value);
+        if (value == null)
+            return null;
+        return isEmpty() ? null : array.swap(draw(), value);
     }
 
     @Override
@@ -46,6 +64,11 @@ public abstract class Bag<G extends S, S, A> extends PushPop.Array<G, S, A> {
         return array.iterator(size);
     }
 
+    /**
+     * A Bag containing Objects removed by random draw
+     * 
+     * @param V the type of the objects in the bag
+     */
     public static class Gen<V> extends Bag<V, V, V[]> {
 
         public Gen(V[] array) {
