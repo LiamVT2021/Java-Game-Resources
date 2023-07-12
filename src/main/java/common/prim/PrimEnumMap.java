1720250e0658485f431537afa18eda85535a68b7
LiamVT2021 <liam.snyder.1998@gmail.com>
2023-07-11 22:30:21 -0400

    add other prim types

--- src/main/java/common/prim/PrimEnumMap.java
+++ src/main/java/common/prim/PrimEnumMap.java
@@ -7,9 +7,50 @@ public interface PrimEnumMap<E extends Enum<E>, N extends Number, A> extends Pri
         return key.ordinal();
     }
 
+    static class Byte<E extends Enum<E>> extends PrimMap.Byte<E> implements PrimEnumMap<E, java.lang.Byte, byte[]> {
+
+        public Byte(Class<E> clazz) {
+            super(clazz.getEnumConstants().length);
+        }
+
+    }
+
+    static class Short<E extends Enum<E>> extends PrimMap.Short<E> implements PrimEnumMap<E, java.lang.Short, short[]> {
+
+        public Short(Class<E> clazz) {
+            super(clazz.getEnumConstants().length);
+        }
+
+    }
+
     static class Int<E extends Enum<E>> extends PrimMap.Int<E> implements PrimEnumMap<E, Integer, int[]> {
 
         public Int(Class<E> clazz) {
+            super(clazz.getEnumConstants().length);
+        }
+
+    }
+
+    static class Long<E extends Enum<E>> extends PrimMap.Long<E> implements PrimEnumMap<E, java.lang.Long, long[]> {
+
+        public Long(Class<E> clazz) {
+            super(clazz.getEnumConstants().length);
+        }
+
+    }
+
+    static class Float<E extends Enum<E>> extends PrimMap.Float<E> implements PrimEnumMap<E, java.lang.Float, float[]> {
+
+        public Float(Class<E> clazz) {
+            super(clazz.getEnumConstants().length);
+        }
+
+    }
+
+    static class Double<E extends Enum<E>> extends PrimMap.Double<E>
+            implements PrimEnumMap<E, java.lang.Double, double[]> {
+
+        public Double(Class<E> clazz) {
             super(clazz.getEnumConstants().length);
         }
 
