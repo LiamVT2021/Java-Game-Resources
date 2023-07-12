69b6aeab8089416cb5b9b7f03a8643f5a2936bb0
LiamVT2021 <liam.snyder.1998@gmail.com>
2023-07-11 22:21:58 -0400

    heaplist from prim branch

---
+++ src/main/java/common/pushPop/HeapList.java
@@ -0,0 +1,69 @@
+package common.pushPop;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import common.prim.PrimHeap;
+
+public class HeapList<V> {
+
+    private final Heap<V, ? super V, ?>[] heaps;
+
+    @SafeVarargs
+    public HeapList(Heap<V, ? super V, ?>... heaps) {
+        this.heaps = heaps;
+    }
+
+    public int size() {
+        return Stream.of(heaps).mapToInt(Heap::size).sum();
+    }
+
+    public int capacity() {
+        return Stream.of(heaps).mapToInt(Heap::capacity).sum();
+    }
+
+    public boolean isEmpty() {
+        return Stream.of(heaps).allMatch(Heap::isEmpty);
+    }
+
+    public boolean isFull() {
+        return Stream.of(heaps).allMatch(Heap::isFull);
+    }
+
+    public boolean push(V value) {
+        for (Heap<V, ? super V, ?> heap : heaps) {
+            if (heap.isFull())
+                continue;
+            heap.push(value);
+            return true;
+        }
+        return false;
+    }
+
+    public HeapList<V> fill(Supplier<V> supplier) {
+        for (Heap<V, ? super V, ?> cur : heaps) {
+            while (!cur.isFull()) {
+                V value = supplier.get();
+                for (Heap<V, ? super V, ?> swap : heaps) {
+                    if (cur == swap)
+                        break;
+                    value = swap.swap(value);
+                }
+                cur.push(value);
+            }
+        }
+        return this;
+    }
+
+    public V swap(V value) {
+        for (Heap<V, ? super V, ?> heap : heaps)
+            value = heap.swap(value);
+        return value;
+    }
+
+    ////////////
+
+    public static HeapList<Integer> middleInt(int dis, int adv) {
+        return new HeapList<Integer>(new PrimHeap.Int(dis, true), new PrimHeap.Int(adv, false));
+    }
+}
