0777f95c23620cbc1b5c7d0827db54e946a49214
LiamVT2021 <liam.snyder.1998@gmail.com>
2023-07-11 22:17:20 -0400

    use paramaterized tests

--- src/test/java/common/pushPop/ArrayWrapperTest.java
+++ src/test/java/common/pushPop/ArrayWrapperTest.java
@@ -4,88 +4,71 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import common.prim.array.*;
 
 /**
  * Tests all methods for Primitive and Generic ArrayWrappers.
  * 
- * @version 6/30/23
+ * @version 7/11/23
  */
 public class ArrayWrapperTest {
 
     private static final int arrSize = 5;
 
-    private GenericArray<Number> numArr;
-    private ByteArray byteArr;
-    private ShortArray shortArr;
-    private IntArray intArr;
-    private LongArray longArr;
-    private FloatArray floatArr;
-    private DoubleArray doubleArr;
-
-    @BeforeEach
-    private void setUp() {
-        numArr = new GenericArray<>(new Number[] { 0, 0, 0, 0, 0 });
-        byteArr = new ByteArray(arrSize);
-        shortArr = new ShortArray(arrSize);
-        intArr = new IntArray(arrSize);
-        longArr = new LongArray(arrSize);
-        floatArr = new FloatArray(arrSize);
-        doubleArr = new DoubleArray(arrSize);
+    private static Stream<ArrayWrapper<? extends Number, Number, ?>> allArrays() {
+        return Stream.of(new GenericArray<>(new Number[] { 0, 0, 0, 0, 0 }),
+                new ByteArray(arrSize), new ShortArray(arrSize), new IntArray(arrSize), new LongArray(arrSize),
+                new FloatArray(arrSize), new DoubleArray(arrSize));
     }
 
-    private void forEach(Consumer<ArrayWrapper<? extends Number, Number, ?>> consumer) {
-        Stream.of(numArr, byteArr, shortArr, intArr, longArr, floatArr, doubleArr).forEach(consumer);
+    @ParameterizedTest
+    @MethodSource("allArrays")
+    public void testCapacity(ArrayWrapper<? extends Number, Number, ?> array) {
+        assertEquals(arrSize, array.capacity());
     }
 
-    @Test
-    public void testCapacity() {
-        forEach(arr -> assertEquals(arrSize, arr.capacity()));
+    @ParameterizedTest
+    @MethodSource("allArrays")
+    public void testGet(ArrayWrapper<? extends Number, Number, ?> array) {
+        assertEquals(0, array.get(0).intValue());
+        assertThrows(IndexOutOfBoundsException.class, () -> array.get(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> array.get(arrSize));
     }
 
-    @Test
-    public void testGet() {
-        forEach(arr -> {
-            assertEquals(0, arr.get(0).intValue());
-            assertThrows(IndexOutOfBoundsException.class, () -> arr.get(-1));
-            assertThrows(IndexOutOfBoundsException.class, () -> arr.get(arrSize));
-        });
-    }
-
-    @Test
-    public void testSwap() {
+    @ParameterizedTest
+    @MethodSource("allArrays")
+    public void testSwap(ArrayWrapper<? extends Number, Number, ?> array) {
         Number n = 2;
-        forEach(arr -> {
-            assertEquals(0, arr.swap(3, n).intValue());
-            assertEquals(n, arr.remove(3).intValue());
-            assertThrows(IndexOutOfBoundsException.class, () -> arr.set(arrSize, n));
-        });
+        assertEquals(0, array.swap(3, n).intValue());
+        assertEquals(n, array.remove(3).intValue());
+        assertThrows(IndexOutOfBoundsException.class, () -> array.set(arrSize, n));
     }
 
-    @Test
-    public void testToString() {
-        String intString = "[ 0, 0, 0, 0, 0 ]";
-        String floatString = "[ 0.0, 0.0, 0.0, 0.0, 0.0 ]";
-        forEach(arr -> assertEquals(arr instanceof PrimArray && ((PrimArray<?, ?>) arr).storesFloat()
-                ? floatString
-                : intString, arr.toString()));
+    @ParameterizedTest
+    @MethodSource("allArrays")
+    public void testToString(ArrayWrapper<? extends Number, Number, ?> array) {
+        assertEquals(array instanceof PrimArray && ((PrimArray<?, ?>) array).storesFloat()
+                ? "[ 0.0, 0.0, 0.0, 0.0, 0.0 ]"
+                : "[ 0, 0, 0, 0, 0 ]",
+                array.toString());
     }
 
     @Test
     public void testPrimStream() {
-        intArr.intStream().forEach(i -> assertEquals(0, i));
-        longArr.longStream().forEach(i -> assertEquals(0, i));
-        doubleArr.doubleStream().forEach(i -> assertEquals(0, i));
+        new IntArray(arrSize).intStream().forEach(i -> assertEquals(0, i));
+        new LongArray(arrSize).longStream().forEach(i -> assertEquals(0, i));
+        new DoubleArray(arrSize).doubleStream().forEach(i -> assertEquals(0, i));
     }
 
     @Test
     public void testGen() {
+        GenericArray<Number> numArr = new GenericArray<>(new Number[] { 0, 0, 0, 0, 0 });
         numArr.forEach(n -> assertEquals(0, n));
         assertEquals(numArr.array, numArr.array());
         for (Number n : numArr)
@@ -94,15 +77,10 @@ public class ArrayWrapperTest {
         assertEquals("[ 0, 0, null, 0, 0 ]", numArr.toString());
     }
 
-    @Test
-    public void testCast() {
-        assertTrue(numArr.cast(arrSize) instanceof Number);
-        assertTrue(byteArr.cast(arrSize) instanceof Byte);
-        assertTrue(shortArr.cast(arrSize) instanceof Short);
-        assertTrue(intArr.cast(arrSize) instanceof Integer);
-        assertTrue(longArr.cast(arrSize) instanceof Long);
-        assertTrue(floatArr.cast(arrSize) instanceof Float);
-        assertTrue(doubleArr.cast(arrSize) instanceof Double);
+    @ParameterizedTest
+    @MethodSource("allArrays")
+    public <N extends Number> void testCast(ArrayWrapper<N, Number, ?> array) {
+        assertTrue(array.cast(arrSize) instanceof N);
     }
 
 }
