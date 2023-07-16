61a1a958e923c8b1f69c67427a8d0ded62d8969b
LiamVT2021 <liam.snyder.1998@gmail.com>
2023-07-16 01:23:59 -0400

    Bag testDraw

--- src/test/java/common/pushPop/BagTest.java
+++ src/test/java/common/pushPop/BagTest.java
@@ -1,13 +1,16 @@
 package common.pushPop;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import static common.pushPop.Bag.GenBag;
 import static common.prim.PrimBag.*;
 
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -32,6 +35,24 @@ public class BagTest {
         fill(bag);
         assertFalse(bag.push(arrSize));
         assertArrayEquals(new int[] { 0, 1, 2, 3, 4 }, bag.stream().mapToInt(Number::intValue).toArray());
+    }
+
+    @Test
+    public void testDraw() {
+        IntBag bag = new IntBag(arrSize) {
+            @Override
+            protected int draw() {
+                return 2;
+            }
+        };
+        assertNull(bag.peek());
+        assertNull(bag.pop());
+        assertNull(bag.swap(null));
+        assertNull(bag.swap(arrSize));
+        fill(bag);
+        assertEquals(2, bag.swap(6));
+        assertEquals(6, bag.pop());
+        assertEquals(4, bag.peek());
     }
 
 }
