9507ce05a21cbbb6348008c4c3677fc0fb7ffb2c
LiamVT2021 <liam.snyder.1998@gmail.com>
2023-07-17 00:25:07 -0400

    loop method

--- src/main/java/common/util/Streamable.java
+++ src/main/java/common/util/Streamable.java
@@ -1,5 +1,7 @@
 package common.util;
 
+import java.util.Iterator;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 @FunctionalInterface
@@ -9,6 +11,26 @@ public interface Streamable<V> extends Iterable<V> {
         Stream.Builder<V> builder = Stream.builder();
         forEach(builder);
         return builder.build();
+    }
+
+    default Supplier<V> loop() {
+        return new Supplier<>() {
+            private boolean isEmpty;
+            private Iterator<V> it;
+
+            @Override
+            public V get() {
+                if (isEmpty)
+                    return null;
+                if (it.hasNext())
+                    return it.next();
+                it = iterator();
+                if (it.hasNext())
+                    return it.next();
+                isEmpty = true;
+                return null;
+            }
+        };
     }
 
 }
