ede3ccca577c0fd3144ff1923174854905d7eac3
LiamVT2021 <liam.snyder.1998@gmail.com>
2023-09-27 01:44:43 -0400

    change to searchable test

--- src/test/java/common/dataStruct/SearchableTest.java
+++ src/test/java/common/dataStruct/SearchableTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
@@ -13,23 +14,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class StreamItTest {
+import common.prim.array.IntArray;
 
-    private static final List<Number> list = List.of(1, 2, 4, 8);
+public class SearchableTest {
 
-    private static Stream<Streamable<Number>> streams() {
-        return Stream.of((Streamable<Number>) list::stream, (IterableExt<Number>) list::iterator);
+    private static final IntArray arr = new IntArray(1, 2, 4, 8);
+
+    private static Stream<Searchable<Integer>> streams() {
+        return Stream.of((IterableExt<Integer>) arr::iterator, (Streamable<Integer>) arr::stream);
     }
+
+    // @Test
+    // public void testSize() {
+    //     assertEquals(4, struct.size());
+    // }
 
     @ParameterizedTest
     @MethodSource("streams")
-    public void testSize(Streamable<Number> stream) {
-        assertEquals(4, stream.size());
-    }
-
-    @ParameterizedTest
-    @MethodSource("streams")
-    public void testGetFirst(Streamable<Number> stream) {
+    public void testGetFirst(Searchable<Integer> stream) {
         assertEquals(4, stream.getFirst(4));
         assertNull(stream.getFirst(n -> n.intValue() > 8));
         assertNull(stream.getFirst(null));
@@ -38,22 +40,22 @@ public class StreamItTest {
 
     @ParameterizedTest
     @MethodSource("streams")
-    public void testHas(Streamable<Number> stream) {
+    public void testHas(Searchable<Integer> stream) {
         assertTrue(stream.has(2));
         assertFalse(stream.has(null));
         assertFalse(stream.has((Object) null));
     }
 
-    @Test
-    public void testLoop() {
-        Supplier<Number> loop = ((IterableExt<Number>) list::iterator).loop();
-        int i = 1, s = 0;
-        while (s++ < 20) {
-            assertEquals(i, loop.get());
-            i = i == 8 ? 1 : i * 2;
-        }
-        Supplier<Object> empty = ((IterableExt<Object>) List.of()::iterator).loop();
-        assertNull(empty.get());
-    }
+    // @Test
+    // public void testLoop() {
+    //     Supplier<Number> loop = struct.loop();
+    //     int i = 1, s = 0;
+    //     while (s++ < 20) {
+    //         assertEquals(i, loop.get());
+    //         i = i == 8 ? 1 : i * 2;
+    //     }
+    //     Supplier<Number> empty = new Struct().loop();
+    //     assertNull(empty.get());
+    // }
 
 }
