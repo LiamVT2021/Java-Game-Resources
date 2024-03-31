b8b3f6584e6b791550f95c2db09c8740efb19a0d
LiamVT2021 <liam.snyder.1998@gmail.com>
2024-03-31 15:14:39 -0400

    page test class

--- src/main/java/common/ui/Page.java
+++ src/main/java/common/ui/Page.java
@@ -10,6 +10,8 @@ public abstract class Page {
     public final String footer;
 
     public Page(String name, String footer) {
+        if (name == null)
+          throw new IllegalArgumentException("Page Name cannot be Null");
         this.name = name;
         this.footer = footer;
     }
@@ -35,6 +37,8 @@ public abstract class Page {
     public static class Single extends Page {
         public Single(String name, Object content, String footer) {
             super(name, footer);
+            if (content == null)
+              throw new IllegalArgumentException("Page Content cannot be Null");
             this.content = content;
         }
     }
---
+++ src/test/java/common/ui/PageTest.java
@@ -0,0 +1,37 @@
+package common.ui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PageTest {
+
+    @Test
+    public void testGetters() {
+        String str = "String";
+        Page page = new Page.Single(str, str, null);
+        assertTrue(str == page.header());
+        assertTrue(str == page.getContent());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Name\n\nContents\nOf\nPage\n\nFooter",
+                new Page.Single("Name", "Contents\nOf\nPage", "Footer").toString());
+        assertEquals("Short\n\nPage\nContent",
+                new Page.Single("Short", "Page\nContent", null).toString());
+    }
+
+    @Test
+    public void testThrows() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
+                () -> new Page.Single(null, null, null));
+        assertEquals("Page Name cannot be Null", thrown.getMessage());
+        thrown = assertThrows(IllegalArgumentException.class,
+                () -> new Page.Single("Name", null, null));
+        assertEquals("Page Content cannot be Null", thrown.getMessage());
+    }
+
+}
