c8bf62b59d4b5bace33431e33a5bd7fb13364dde
LiamVT2021 <liam.snyder.1998@gmail.com>
2025-03-11 22:40:10 -0400

    BoolMethodSet

--- src/main/java/common/ui/data/func/BoolMethodSet.java
+++ src/main/java/common/ui/data/func/BoolMethodSet.java
@@ -2,31 +2,36 @@ package common.ui.data.func;
 
 import java.util.stream.Stream;
 
-public class BoolMethodSet<I> implements IntMethod<I> {
+public class BoolMethodSet<I> {
     private final BoolMethod<I>[] methods;
+    public final BoolMethod<I> any, all;
+    public final IntMethod<I> count;
 
     public BoolMethodSet(BoolMethod<I>[] methods) {
         this.methods = methods;
+        any = new Any();
+        all = new All();
+        count = new Count();
     }
 
-    @Override
-    public int applyAsInt(I input) {
-        return (int) Stream.of(methods).filter(m -> m.test(input)).count();
-    }
-
-    @Override
-    public Result<? extends Method<?, ?>, I, Integer> results(I input) {
-        return new BoolSetResult(input, Stream.of(methods).map(m -> m.results(input)).toArray(Result[]::new));
-    }
-
-    private final class BoolSetResult extends Result<BoolMethodSet<I>, I, Integer> {
-        public BoolSetResult(I input, Result<?, ?, ?>[] children) {
-            super(input, null, (int) Stream.of(children).filter(r -> (boolean) r.output).count(), children);
-        }
-
+    private final class Any implements BoolMethod<I> {
         @Override
-        public BoolMethodSet<I> method() {
-            return BoolMethodSet.this;
+        public boolean test(I input) {
+            return Stream.of(methods).anyMatch(m -> m.test(input));
+        }
+    }
+
+    private final class All implements BoolMethod<I> {
+        @Override
+        public boolean test(I input) {
+            return Stream.of(methods).allMatch(m -> m.test(input));
+        }
+    }
+
+    private final class Count implements IntMethod<I> {
+        @Override
+        public int applyAsInt(I input) {
+            return (int) Stream.of(methods).filter(m -> m.test(input)).count();
         }
     }
 
